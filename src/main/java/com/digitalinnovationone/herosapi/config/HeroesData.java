0874307d3f38357784cbf755a7acc094395cfccf
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-03-13 23:04:24 -0300

    projeto finalizado

---
+++ src/main/java/com/digitalinnovationone/herosapi/config/HeroesData.java
@@ -0,0 +1,59 @@
+package com.digitalinnovationone.herosapi.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import static com.digitalinnovationone.herosapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
+import static com.digitalinnovationone.herosapi.constants.HeroesConstant.REGION_DYNAMO;
+
+public class HeroesData {
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+            .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Heroes_Table_Demo");
+
+        Item hero = new Item()
+                .withPrimaryKey("id", "2")
+                .withString("name", "Mulher maravilha")
+                .withString("universe", "dc comics")
+                .withNumber("films", 3);
+
+        Item hero2 = new Item()
+                .withPrimaryKey("id", "3")
+                .withString("name", "Viuva negra")
+                .withString("universe", "marvel")
+                .withNumber("films", 2);
+
+        Item hero3 = new Item()
+                .withPrimaryKey("id", "4")
+                .withString("name", "Capita marvel")
+                .withString("universe", "marvel")
+                .withNumber("films", 2);
+
+        PutItemOutcome outcome1 = table.putItem(hero);
+        PutItemOutcome outcome2 = table.putItem(hero2);
+        PutItemOutcome outcome3 = table.putItem(hero3);
+    }
+}
---
+++ src/main/java/com/digitalinnovationone/herosapi/config/HeroesTable.java
@@ -0,0 +1,43 @@
+package com.digitalinnovationone.herosapi.config;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import static com.digitalinnovationone.herosapi.constants.HeroesConstant.REGION_DYNAMO;
+import static com.digitalinnovationone.herosapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
+import java.util.Arrays;
+
+public class HeroesTable {
+
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+            .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "Heroes_Table_Demo";
+
+        try {
+            Table table = dynamoDB.createTable(tableName,
+                Arrays.asList(new KeySchemaElement( "id", KeyType.HASH)),
+                Arrays.asList(new AttributeDefinition("id", ScalarAttributeType.S)),
+                new ProvisionedThroughput(5L, 5L));
+
+            table.waitForActive();
+            System.out.println("Sucesso " + table.getDescription().getTableStatus());
+        }
+        catch (Exception e) {
+            System.err.println("Não foi possível criar a tabela");
+            System.err.println(e.getMessage());
+        }
+    }
+}
